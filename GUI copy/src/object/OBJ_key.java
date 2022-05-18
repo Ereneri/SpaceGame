d7b407e718d1537810081536a0505ef967194abb
LucWillett <64013003@ep-student.org>
2022-05-18 13:38:24 -0500

    Object view

---
+++ GUI copy/src/object/OBJ_key.java
@@ -0,0 +1,9 @@
+package object;
+
+public class OBJ_key {
+
+	public OBJ_key() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- GUI copy/src/object/metal.java
+++ GUI copy/src/object/metal.java
@@ -1,6 +1,9 @@
 package object;
 
+import java.awt.image.BufferedImage;
+
 public class metal {
+	public BufferedImage image;
 	public boolean collision = false; 
 	public int worldX, worldY;
     public int getValue() {
