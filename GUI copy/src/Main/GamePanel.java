299c036a50afa2e2e21ce1b04ce181a51f91a7c3
FunLuke <105012878+FunLuke@users.noreply.github.com>
2022-05-20 13:24:16 -0500

    bullet coliding with asteroid (kinda)

--- GUI copy/src/Main/GamePanel.java
+++ GUI copy/src/Main/GamePanel.java
@@ -13,6 +13,7 @@ import asteroids.*;
 import object.metal;
 import rocketship.bullet;
 import rocketship.rocketship;
+import rocketship.rocketship.bulletsClass;
 
 public class GamePanel extends JPanel implements Runnable {
     // Basic Screen Vars and scale factor
@@ -137,8 +138,16 @@ public class GamePanel extends JPanel implements Runnable {
         }
         
         for(int i = 0; i<ast.asts.length; i++) {
-        	ast.asts[i].astTick();
-        	ast.asts[i].draw(g2);
+        	if(ast.asts[i]!= null) {
+        		ast.asts[i].astTick();
+            	ast.asts[i].draw(g2);
+            	for (int ibull = 0; ibull < bulletsClass.bullets.size(); ibull++) {
+            		if(ast.asts[i].getCAst().touches(bulletsClass.bullets.get(ibull).getBulletC())) {
+            			bulletsClass.bullets.remove(ibull);
+            			System.out.println("touch");
+                	}
+                }
+        	}
         }
 
         ship.draw(g2);
