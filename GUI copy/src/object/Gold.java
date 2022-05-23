e25fc4f7f8314fa5517cf111b1717da1be753a29
LucWillett <64013003@ep-student.org>
2022-05-23 13:37:33 -0500

    Cleanup

--- GUI copy/src/object/Gold.java
+++ GUI copy/src/object/Gold.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import collision.Collision;
-
 public class Gold extends metal{
 	
 	public Gold() {
 		name = "Gold";
 		value = 100;
 		
+		//Places an image on object
 		try {
 			image = ImageIO.read(getClass().getResourceAsStream("/object/Gold.png"));
 		} catch (IOException e) {
--- GUI copy/src/object/Iron.java
+++ GUI copy/src/object/Iron.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import collision.Collision;
-
 public class Iron extends metal{
 	
 	public Iron() {
 		name = "Iron";
 		value = 25;
 		
+		//Places an image on object
 		try {
 			image = ImageIO.read(getClass().getResourceAsStream("/object/Iron.png"));
 		} catch (IOException e) {
